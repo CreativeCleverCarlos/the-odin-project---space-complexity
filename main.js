9234d9e491974f72e43bda849dc46842ffd92321
Carlos Campbells Laptop <carlos.g.campbell@gmail.com>
2024-05-01 13:10:14 -0400

    notes - for more detailed just reread the site

---
+++ main.js
@@ -0,0 +1,7 @@
+//Space complxity is the totla space used by an algorithm relative to the size of the unit
+
+//in lame mans terms, it's the total amount of working memory the algorithm needs
+
+//it's measured in the same way as time complexity - using the Big O (worst case scenario to best case scenario).
+
+//Only, it's focused on memory space rather than time
